3661e603fcc4cbfd4a5707f92d70e230e34eb7d4
Jett Thompson <jett.thompson.59@gmail.com>
2020-05-24 12:19:30 +1000

    added the content of the new class

--- src/com/mycodefu/birthday/java/SoundLoader.java
+++ src/com/mycodefu/birthday/java/SoundLoader.java
@@ -1,5 +1,19 @@
 package com.mycodefu.birthday.java;
 
-public class SoundLoader {
+import java.io.File;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class SoundLoader {
+public static Clip loadSoundFile(String path) {
+	try {
+		var in = AudioSystem.getAudioInputStream(new File(path));
+		var clip = AudioSystem.getClip();
+		clip.open(in);
+		return clip;
+	}catch(Exception e) {
+		throw new IllegalArgumentException(e);
+	}
+}
 }
